35ea9191eb06a84a9f987aa027f7f53afc9d163d
higgyone <higgyone@hotmail.com>
2017-01-23 21:35:10 +0000

    Added temperature sensor kalman filter and test agains low pass filter. Kalman better but not good with fast changing measurements as A = 1 and previous prediction should be the same as the next one

--- KalmanFilter/Main.py
+++ KalmanFilter/Main.py
@@ -23,19 +23,48 @@ xKVals = []
 yKVals = []
 yKRandVals = []
 
-kal.SetInitalXandP(14, 6)
+# set the initial estimate at 5oC and prediction at 2
+kal.SetInitalXandP(10,2)
 
-for i in frange (0.0,10.0, 0.2):
+# for simple temperature sensor:
+# A = 1 - expect the next estimate to be roughly the same as the previous, as long as it doesnt change too fast
+# H = 1 - estimate and measurement are the same units, no need to convert
+# Q = 0.1 - noise covariance in state transition prediction
+# R = 0.5 - measurement noise covariance
+kal.SetAHQR(1,1,0.01,0.5)
+
+increase = 0.0
+actualNoNoise = []
+lowPass = []
+
+for i in frange (0.2,100.0, 0.2):
     xKVals.append(i)
 
-    z = testGenerator.GetRandomFixedvalueLimits(14.4, 4)
+    increase += 1/i
+    actualNoNoise.append(increase)
+    z = testGenerator.GetRandomFixedvalueLimits(0,2) + increase
     yKRandVals.append(z)
+    lowPass.append(lpf.LowPass1stOrderFilter(z))
+    temperature = kal.SimpleKalman(z)
+    yKVals.append(temperature)
 
-    volt = kal.SimpleKalman(z)
-    yKVals.append(volt)
-
-pyplot.plot(xKVals, yKVals, 'o-', xKVals, yKRandVals, 'r:*')
+pyplot.plot(xKVals, yKVals, 'b-', xKVals, yKRandVals, 'r-', xKVals, actualNoNoise, 'g-', xKVals, lowPass, 'y-')
 pyplot.show()
+
+#**************Simple Kalman fixed voltage**************
+#kal.SetInitalXandP(14, 6)
+
+#for i in frange (0.0,10.0, 0.2):
+#    xKVals.append(i)
+
+#    z = testGenerator.GetRandomFixedvalueLimits(14.4, 4)
+#    yKRandVals.append(z)
+
+#    volt = kal.SimpleKalman(z)
+#    yKVals.append(volt)
+
+#pyplot.plot(xKVals, yKVals, 'o-', xKVals, yKRandVals, 'r:*')
+#pyplot.show()
 
 #**************Test AveFilter**************
 #for i in range (0,9):
